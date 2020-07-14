9a072e9c5ecb98f036a46784b53f1749390440c5
ilham <ilhamksyuriadi@gmail.com>
2020-07-15 01:11:37 +0700

    adding form username

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/component/clearable-input/clearable-input.component.ts
@@ -0,0 +1,13 @@
+// import {Component} from '@angular/core';
+
+// /**
+//  * @title Input with a clear button
+//  */
+// @Component({
+//   selector: 'clearable-input',
+//   templateUrl: './clearable-input.component.html',
+//   styleUrls: ['./clearable-input.component.scss'],
+// })
+// export class ClearableInput {
+//   value = 'Clear me';
+// }
