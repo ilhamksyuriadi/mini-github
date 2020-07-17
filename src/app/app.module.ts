b872e3f620a2a8ed9f05f13f265ddac37d909124
ilham <ilhamksyuriadi@gmail.com>
2020-07-17 18:35:23 +0700

    set up ngxs state and adding state

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
+import { UserState } from './state/user/user.state';
+import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,7 +52,12 @@ import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.compo
     MatExpansionModule,
     MatMenuModule,
     MatPaginatorModule,
-    MatTableModule
+    MatTableModule,
+    NgxsModule.forRoot([
+      UserState
+    ]),
+    NgxsReduxDevtoolsPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { api_url } from '../config/api-url';
 import { Router } from '@angular/router';
+import { Store } from '@ngxs/store';
+import { AddUser } from '../state/user/user.action';
 
 @Component({
   selector: 'app-home',
@@ -16,9 +18,10 @@ export class HomeComponent implements OnInit {
   public value : String = '';
   public is_loading : boolean = false;
 
-  constructor ( private http: HttpClient, private router: Router ) { }
+  constructor ( private http: HttpClient, private router: Router, private store: Store ) { }
 
   ngOnInit () : void {
+    this.addUser('saprul')
   }
 
   submit () {
@@ -41,10 +44,14 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  getUser (username) {
+  getUser(username) {
     let url = api_url.user;
     url = url.replace('[user]', username);
     return this.http.get(url);
+  }
+
+  addUser(name) {
+    this.store.dispatch(new AddUser({name: name}))
   }
 
 }
